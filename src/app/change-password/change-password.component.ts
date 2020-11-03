4bc1b84974382c349315cdbbac14101a6c3d3093
Nixon Azeredo <39866741+NixonAzeredo@users.noreply.github.com>
2020-11-03 17:52:44 -0300

    FIX: Adiciona validacao no formulario e roteamento de sucesso

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,24 +10,52 @@ import { AuthService } from '../auth.service';
 })
 export class ChangePasswordComponent implements OnInit {
 
+  message = '';
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) { }
 
 
   passwordForm = this.fb.group({
-    oldPassword: [''],
-    newPassword: ['']
+    oldPassword: ['', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+      Validators.pattern('')
+    ]],
+    newPassword: ['', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+      Validators.pattern('')
+    ]]
   });
 
   ngOnInit() {
   }
 
+  get validate() {
+    return this.passwordForm.status === 'INVALID';
+  }
+
   changePassword(): void {
     const pass = this.passwordForm.getRawValue();
     const { idToken } = JSON.parse(localStorage.getItem("ACCESS_TOKEN"));
-    this.authService.changePassword(pass.oldPassword, pass.newPassword, idToken.payload.email);
+    this.authService.changePassword(pass.oldPassword, pass.newPassword, idToken.payload.email).then(
+      result => {
+        this.message = "Troca realizada com sucesso";
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 1500);
+      }
+    ).catch(error => {
+      this.message = error.message;
+      this.passwordForm.patchValue({
+        oldPassword: ''
+      });
+    });
   }
 
 }
