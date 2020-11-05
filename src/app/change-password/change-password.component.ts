39c14bee9aaa112ce28da98f68c18e8deb181af8
Nixon Azeredo <39866741+NixonAzeredo@users.noreply.github.com>
2020-11-05 15:07:35 -0300

    FEATURE: Adiciona strength password e score

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -48,6 +48,40 @@ export class ChangePasswordComponent implements OnInit {
     return this.passwordForm.status === 'INVALID';
   }
 
+  checkPasswordStrength(validator: string): boolean {
+    const list = {
+      lower: /([a-z])/g,
+      upper: /([A-Z])/g,
+      number: /([0-9])/g,
+      special: /[^A-z\s\d][\\\^]?/g,
+      eight: /^.{8,}$/g
+    };
+    const { password } = this.passwordForm.getRawValue();
+    const regex = RegExp(list[validator]);
+    return regex.test(password);
+  }
+
+  strengthScore(): number {
+    let score = 0;
+    if (this.checkPasswordStrength('lower')) {
+      score += 20;
+    }
+    if (this.checkPasswordStrength('upper')) {
+      score += 20;
+    }
+    if (this.checkPasswordStrength('number')) {
+      score += 20;
+    }
+    if (this.checkPasswordStrength('special')) {
+      score += 20;
+    }
+    if (this.checkPasswordStrength('eight')) {
+      score += 20;
+    }
+
+    return score;
+  }
+
   changePassword(): void {
     const pass = this.passwordForm.getRawValue();
     const { idToken } = JSON.parse(localStorage.getItem("ACCESS_TOKEN"));
